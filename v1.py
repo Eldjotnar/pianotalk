cd57ab3ff9b999e5c640817eda2d0a9690733300
Andreas Raduege <andrew.raduege@gmail.com>
2016-12-02 08:30:06 -0600

    return frequencies and scaled volume

--- v1.py
+++ v1.py
@@ -20,19 +20,18 @@ def getFrequencies(y, Fs):
 	
 	freqs = []
 	vol = []
+	maxi = max(np.abs(Y))
+
 	#pick out the frequencies above the threshold
-	max = np.argmax(np.abs(Y))
  	for i in range(0,n):
  		if abs(Y[i]) > THRES:
  			freqs.append(abs(find_freqs[i]*Fs))
- 			vol.append(scale(abs(Y[i]),THRES,max))
- 			#print("vol is " + str(vol[-1]))
- 			#print(abs(find_freqs[i]*Fs)) #freqeuncies in Hz
- 	#print("There are " + str(len(freqs)))
+ 			vol.append(scale(abs(Y[i]),THRES,maxi))
+ 	assert len(freqs) == len(vol)
 
 #scales a value between 1 and 127
 def scale(x, min, max):
- 	return (127 * (x - min))/(max - min) + 1
+ 	return int(round(127 * (x - min))/(max - min))
 
 def main():
 	#read in audio and select channel
@@ -43,27 +42,20 @@ def main():
 	sampSize = (Fs * WINDOW_LEN)/2
 	prevStart = 0
 	end = int(sampSize)
-	totalSamp = (len(y)/(sampSize*2) *2) #number of times to loop
+	totalSamp = int(len(y)/(sampSize*2) *2) #number of times to loop
 
 	#iterate over the data
 	for j in range(0,totalSamp):
 		#print("Getting frequencies between " + str(prevStart/Fs) + " and " + str(end/Fs))
-		#print("Getting frequencies between " + str(prevStart) + " and " + str(end))
 		getFrequencies(y[prevStart:end],Fs)
 		prevStart += sampSize
 		end += sampSize
-
-	#print(sampSize)
 
 	#plot the incoming audio signal
 	subplot(2,1,1)
 	plot(data[:1000])
 	xlabel('Time')
 	ylabel('Amplitude')
-
-	#plot the power spectrum
-	subplot(2,1,2)
-	#getFrequencies(y,Fs)
 	show()
 
 if __name__ == '__main__':
